4ff3491baac613f8acf1f46f90e11cf076351093
Greg Lim <limjunqu@gmail.com>
2019-05-27 10:11:35 +0800

    Update Product.js

--- Product.js
+++ Product.js
@@ -12,15 +12,19 @@ class Product extends Component {
     return (
       <div>                
         <Media>
-        <Media.Left>
-            <img width={64} height={64} src={this.props.data.imageUrl} alt="Image"/>
-        </Media.Left>        
-        <Media.Body>
-            <Media.Heading>{this.props.data.productName}</Media.Heading>
-            { this.props.data.releasedDate }
-            <Rating rating={this.props.data.rating} numOfReviews={this.props.data.numOfReviews}/>
-            <p>{this.props.data.description}</p>
-        </Media.Body>
+            <img 
+                width={64} 
+                height={64} 
+                className="mr-3"
+                src={this.props.data.imageUrl} 
+                alt="Image"
+            />
+            <Media.Body>
+                <h5>{this.props.data.productName}</h5>
+                { this.props.data.releasedDate }
+                <Rating rating={this.props.data.rating} numOfReviews={this.props.data.numOfReviews}/>
+                <p>{this.props.data.description}</p>
+            </Media.Body>
         </Media>                                                                                                                                   
       </div>
     );
