93e5bc67f788b9a5e4a8ec629a280d6f128efee1
Eugene Heynike <eugeneheynike@icloud.com>
2022-10-19 15:47:58 +0200

    chapter two done

---
+++ src/Product.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+
+
+
+
+
+class Products extends Component {
+    render() {
+        const products = ["Learning React","Pro React","Beginning React"]; 
+        const listProducts = products.map((product) =>
+            <li key={product.toString()}>{product}</li> );
+
+        return (
+            <div>
+                <h2>
+                    Products
+                    <ul>{listProducts}</ul>
+                </h2>
+            </div>
+        )
+    }
+}
+
+export default Products;
